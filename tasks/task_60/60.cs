f911b9c57f46acef0169569a428ba970463be870
Mariia <>
2022-06-07 22:30:13 +0300

    решение задачи 60

---
+++ tasks/task_60/60.cs
@@ -0,0 +1,31 @@
+﻿// : Сформируйте трёхмерный массив из неповторяющихся
+//двузначных чисел. Напишите программу, которая будет построчно выводить
+//массив, добавляя индексы каждого элемента.
+
+Console.WriteLine("Введите количество строк по ширине массива: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов по высоте массива: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк в глубину массива: ");
+int z = int.Parse(Console.ReadLine());
+
+int[,,] GetArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rnd.Next(0, 11);
+                Console.Write(array[i, j, k] + "(" + i + j + k + ")" + " ;");
+            }
+            Console.WriteLine();
+        }
+    }
+    return array;
+}
+GetArray(x, y, z);
+
