0e3f7c477b412b537cd2c8f1c3430e7a2827ab0b
Mariia <>
2022-06-09 18:51:59 +0300

    теперь решение универсальное

--- tasks/task_58/58.cs
+++ tasks/task_58/58.cs
@@ -1,14 +1,18 @@
 ﻿//Задайте две матрицы. Напишите программу, которая будет
 //находить произведение двух матриц.
 
-Console.WriteLine("ВВедите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine());
-Console.WriteLine("ВВедите количество столбцов массива: ");
-int cols = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите количество строк первого массива массива: ");
+int rows1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите количество столбцов первого массива массива: ");
+int cols1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите количество строк второго массива массива: ");
+int rows2 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите количество столбцов второго массива массива: ");
+int cols2 = int.Parse(Console.ReadLine());
 
-int[,] GetArray(int row, int col)
+int[,] GetArray(int row1, int col1)
 {
-    int[,] array = new int[row, col];
+    int[,] array = new int[row1, col1];
     Random rnd = new Random();
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -31,17 +35,16 @@ void PrintArray(int[,] array)
         Console.WriteLine();
     }
 }
-int[,] arrayResult = GetArray(rows, cols);
+int[,] arrayResult = GetArray(rows1, cols1);
 Console.WriteLine("Исходный массив1:");
 PrintArray(arrayResult);
-int[,] arrayResult2 = GetArray(rows, cols);
+int[,] arrayResult2 = GetArray(rows2, cols2);
 Console.WriteLine("Исходный массив2:");
 PrintArray(arrayResult2);
 
 int[,] GetMultiplication(int[,] arrayResult, int[,] arrayResult2)
 {
-    int[,] resultMatrix = new int[rows, cols];
-    
+    int[,] resultMatrix = new int[rows1, cols2];
     for (int i = 0; i < arrayResult.GetLength(0); i++)
     {
         for (int j = 0; j < arrayResult2.GetLength(1); j++)
@@ -49,14 +52,20 @@ int[,] GetMultiplication(int[,] arrayResult, int[,] arrayResult2)
             for (int k = 0; k < arrayResult.GetLength(1); k++)
             {
                 resultMatrix[i, j] += arrayResult[i, k] * arrayResult2[k, j];
-                
             }
-            
         }
     }
     return resultMatrix;
 }
-GetMultiplication(arrayResult, arrayResult2);
-Console.WriteLine();
-int[,] getResult = GetMultiplication(arrayResult, arrayResult2);
-PrintArray(getResult);
+
+if (rows1 == cols2)
+{
+    GetMultiplication(arrayResult, arrayResult2);
+    Console.WriteLine("Результат умножения двух матриц");
+    int[,] getResult = GetMultiplication(arrayResult, arrayResult2);
+    PrintArray(getResult);
+}
+else
+{
+    Console.WriteLine("Матрицы таких размеров цмножить невозможно");
+}
